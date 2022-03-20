23ac6d4157d60614371b45b12079f038cd0d565e
2018007983 <2018007983@hanyang.ac.kr>
2022-03-21 01:34:58 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,162 +2,127 @@ import copy
 from pprint import pprint as pp
 from itertools import combinations #부부집합 구하기 위한 라이브러리
 
-def find_first_scan(fin):
-    first_scan = {}
-    total_list = []
+global total_list, min_support
+
+total_list = []
+min_support = 0
+def find_first_scan(fin, frequent):
+    global total_list, min_support
+
     for line in fin:
         transaction = set()
         arr = line.strip('\n').split('\t')
         for item in arr:
+            item = int(eval(item))
             transaction.add(item)
-            if item in first_scan:
-                first_scan[item] = first_scan.get(item) + 1
+            if item in frequent:
+                frequent[item] = frequent.get(item) + 1
             else:
-                first_scan[item] = 1
+                frequent[item] = 1
         total_list.append(transaction)
     print("###total list", total_list)
-    pp("print", first_scan)
-    return first_scan
-
-def cal_support(scan, row_count, min_support):
-
-    for i in scan:
-        scan[i] = (scan[i]/row_count)*100
-    del_scan = []
-    del_list = []
-    for j in scan.keys():
-        if scan[j] < min_support:
-            del_scan.append(j)
-            del_list.append(set(j))
-
-    for del_key in del_scan:
-        del(scan[del_key])
-    pp(scan)
-
-    print("del scan : ", del_list)
-    return del_list
-
-def cal_union(scan, del_scan, order):
-    list_key = []
-    for i in scan.keys():
-        list_key.append(i)
-    print(list_key)
-
-    new_scans = {}
-    del_scan_update = []
-    new_union_list = []
-    for j in range(len(list_key)-1):
-        for k in range(j+1, len(list_key)):
-            set_keys1 = set(list_key[j])
-            set_keys2 = set(list_key[k])
-            #print("set key", set_keys1, set_keys2)
-            ss = set_keys1.union(set_keys2)
-            if len(ss) > order: #order이 현재 각 set의 원소의 개수를 말한다. 더 많으면 order개로 쪼개야한다.
-                tmp_list = list(combinations(ss, order))
-                print("larger than order ", ss)
-                print("tmp_list ", tmp_list)
-                for tmp in tmp_list:
-                    new_union_list.append(set(tmp))
-            #print(ss) #합집합 잘 되었음
-            else :
-                new_union_list.append(set(ss))
-    return new_union_list
-
-def prunning_set(new_union_list, del_set, order):
-
-    prunning_union = set()
-    prunning_list = []
-    after_prunning_list = []
-    print("rrrrppruning set new ", new_union_list, del_set)
-    for i in range(len(new_union_list)):
-        for l in range(len(del_set)):
-            if del_set[l].issubset(new_union_list[i]):
-                print("issubset ", del_set[l])
-                break
-            else:
-                if l == len(del_set)-1:
-                    print("here", new_union_list[i])
-                    prunning_list.append(new_union_list[i])
-
-    print("after prunning ", prunning_list)
 
 
-            #sub_list = set(combinations(new_union_list[i], order-1)) #한 행씩 부분집합을 만들어서 del_set인 것이 있는지 확인
-            # for l in range(len(sub_list)):
-            #     for ll in range(len(del_set[ll])):
-            #         if del_set[ll]
-            # if sub_list.issubset(new_union_list[i]):
-            #     continue
-            # else:
-            #     prunning_list.append(new_union_list[i])
+    for freq in frequent:
+        frequent[freq] = round((frequent[freq]/len(total_list))*100,2)
 
-    tmp_list = copy.deepcopy(prunning_list)
-    count = 0
-    for j in range(len(tmp_list)):
-        for k in range(len(prunning_list)):
-            if len(tmp_list[j].intersection(prunning_list[k])) == order:
-                #print("len tmp", len(tmp_list[j].intersection(prunning_list[k])))
-                count += 1
-                if count >= 2:
-                    del prunning_list[k]
-                    break
-        count = 0
-    # set->list->set
-    print("prunning ", prunning_list)
-    return prunning_list
+    new_frequent = {key: value for key, value in frequent.items() if value >= min_support}
+    pp(new_frequent)
+    return new_frequent
 
 
-def count_in_total(total_list, new_union_list):
-    new_scan = {}
-    for i in range(len(total_list)):
-        for j in range(len(new_union_list)):
-            if new_union_list[j].issubset(total_list[i]):
-                tmp = tuple(new_union_list[j])
-                if tmp in new_scan:
-                    new_scan[tmp] = new_scan.get(tmp) + 1
+def frequent_count(list):
+    global total_list
+    new_frequent = {}
+    for total_row in total_list:
+        for list_item in list:
+            list_set = set(list_item)
+            if list_set.issubset(total_row):
+                make_tuple = tuple(list_set)
+                if make_tuple in new_frequent: #튜플 형태로 new_frequent에 넣기
+                    new_frequent[make_tuple] = new_frequent[make_tuple] + 1
                 else:
-                    new_scan[tmp] = 1
-    return new_scan
+                    new_frequent[make_tuple] = 1
+    print("new" , new_frequent)
+
+    for freq in new_frequent:
+        new_frequent[freq] = round((new_frequent[freq]/len(total_list))*100,2)
+
+    change_frequent = {key: value for key, value in new_frequent.items() if value >= min_support}
+    print("change", change_frequent)
+    print(len(change_frequent))
+    if len(change_frequent) < 0:
+        print("no")
+        exit()
+    return change_frequent
+
+def join_and_pruning(frequent, order):
+    new_frequent_list = []
+    list_key = []
+    for i in frequent.keys():
+        if isinstance(i, tuple):
+            i = set(i)
+        list_key.append(i)
+
+    print(list_key)
+    for j in range(len(list_key) - 1):
+        for k in range(j + 1, len(list_key)):
+            set_keys1 = list_key[j]
+            set_keys2 = list_key[k]
+            if isinstance(set_keys1, int):
+
+                tmp = set()
+                tmp.add(list_key[j])
+                set_keys1 = tmp
+                tmp2 = set()
+                tmp2.add(list_key[k])
+                set_keys2 = tmp2
+            else:  # order가 2일때 한개씩 합치는것
+                set_keys1 = set(list_key[j])
+                set_keys2 = set(list_key[k])
+
+            union = set_keys1.union(set_keys2)
+            subset_list = list(combinations(union, order))
+            for l in subset_list:
+                change_set = set(l)
+                cnt = 0
+                for key_set in list_key:
+                    if isinstance(key_set, int):
+                        temp = set()
+                        temp.add(key_set)
+                        key_set = temp
+                    if key_set.issubset(change_set):
+                        cnt += 1
+                        if cnt == order and change_set not in new_frequent_list:
+                            new_frequent_list.append(change_set)
+                            break
+
+    print(new_frequent_list)
+    return new_frequent_list
+
+
 def main():
+    global min_support
     fin = open('input.txt', "r")
     fout = open('output.txt', "w")
 
-    min_support = 50
-    row_count = 0
+    min_support = 5
+    frequent = {}
+    frequent = find_first_scan(fin, frequent)
+    pp(frequent)
 
-    first_scan = {}
-    total_list = []
-    for line in fin:
-        transaction = set()
-        arr = line.strip('\n').split('\t')
-        for item in arr:
-            transaction.add(item)
-            if item in first_scan:
-                first_scan[item] = first_scan.get(item) + 1
-            else:
-                first_scan[item] = 1
-        row_count += 1
-        total_list.append(transaction)
-    pp(first_scan)
-
-    del_scan = cal_support(first_scan, row_count, min_support)
-
-    new_scan = copy.deepcopy(first_scan)
-    order = 1
+    order = 2
     while True:
+        list = join_and_pruning(frequent,order)
+        if len(list) == 0:
+            print("break")
+            break
+        elif len(list) == 1:
+            print("here", list)
+            break
+        frequent = frequent_count(list)
+        #print(frequent)
         order += 1
-        length = len(new_scan)
-        if length == 0:
-            print("length is 0")
-            break
-        elif length == 1:
-            print("answer is ", new_scan)
-            break
-        new_union_list = cal_union(new_scan, del_scan, order)
-        new_scan = count_in_total(total_list, prunning_set(new_union_list,del_scan,order))
-        pp(new_scan)
-        del_scan = cal_support(new_scan, row_count, min_support)
-
 
     fin.close()
     fout.close()
